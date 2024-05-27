7beeec59d737b2263e4d514042cb0ef63d1a660f
AmirAliAzimloo <amiraliazimloo123@gmail.com>
2024-05-27 22:50:07 +0430

    Add router and config routs.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import {
+  GsapTo,
+  Home,
+} from "./pages";
 
 const App = () => {
   return (
-    <div className="text-green-700">
-      Hello World !
+    <div className="bg-black min-h-screen w-full">
+      <Router>
+        <Routes>
+          <Route path="/gsapto" element={<GsapTo />} />
+          {/* <Route path="/gsapfrom" element={<GsapFrom />} />
+          <Route path="/gsapfromto" element={<GsapFromTo />} />
+          <Route path="/gsaptimeline" element={<GsapTimeline />} />
+          <Route path="/gsapstagger" element={<GsapStagger />} />
+          <Route path="/gsapscrolltrigger" element={<GsapScrollTrigger />} />
+          <Route path="/gsaptext" element={<GsapText />} /> */}
+          <Route path="/" element={<Home />} /> 
+        </Routes>
+      </Router>
     </div>
   )
 }
